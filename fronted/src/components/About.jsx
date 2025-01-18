8d24f9c7281bf25d83ac8e0bba9d9270e2236f13
Ramakrishna T <147640669+RamakrishnaTanam@users.noreply.github.com>
2025-01-18 10:58:07 +0530

    Update About.jsx

--- fronted/src/components/About.jsx
+++ fronted/src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { HiOutlineArrowRight } from "react-icons";
+import { HiOutlineArrowRight } from "react-icons/hi";
 
 const About = () => {
   return (
